ee07cf3a7a92a808fc0b70d322e2b2c978cc9701
kamaladebayo <60827461+kamaladebayo@users.noreply.github.com>
2022-06-05 11:38:59 +0100

    Final updates

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ document.getElementById("rate").addEventListener('change', (e) => {
 
 
 
-document.getElementById('submitBtn').addEventListener('click', (e) => {
+document.getElementById('interest-form').addEventListener('submit', (e) => {
     // declare variables
     let result = document.getElementById("result")
     let principal = document.getElementById("principal").value
     let rate = document.getElementById("rate").value
     let years = parseInt(document.getElementById("years").value)
-    let amount = principal * (1 + ((rate/100) * years))
+    let amount = principal * (1 + ((rate/100) * years)).toFixed(2)
 
     e.preventDefault()
+    if(!principal){
+        return;
+    }else if(principal < 0){
+        result.innerHTML = `Input can't be negative`
+    }else{
+        // set result
+        result.innerHTML = `If you deposit <mark>${principal}</mark>, <br /> at an interest rate of <mark>${rate}</mark>.<br /> You will receive an amount of <mark>${(amount - principal).toFixed(2)}</mark> <br /> in the year <mark>${2022 + years}</mark>`
+    }
 
-    // set result
-    result.innerHTML = `If you deposit ${principal}, <br /> at an interest rate of ${rate}.<br /> You will receive an amount of ${amount - principal} <br /> in the year ${2022 + years}`
     
 })
